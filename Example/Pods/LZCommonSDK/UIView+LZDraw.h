5115c97b9070378f26aed3932209b2ab78eae81a
Macye <linzhenwei8888@gmail.com>
2018-08-27 11:34:43 +0800

    新功能

--- Example/Pods/LZCommonSDK/UIView+LZDraw.h
+++ Example/Pods/LZCommonSDK/UIView+LZDraw.h
@@ -7,6 +7,11 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum : NSUInteger {
+    LZDrawStyleNormal,
+    LZDrawStyleFill,
+} LZDrawStyle;
+
 @interface UIView (LZDraw)
 
 
@@ -30,6 +35,44 @@
  @param color 颜色
  @param font 字体
  */
-- (void)drawText:(NSString*)text inFrame:(CGRect)frame aligment:(NSTextAlignment)aligment color:(UIColor*)color font:(UIFont*)font;
+- (void)drawText:(NSString*)text
+         inFrame:(CGRect)frame
+            with:(CGFloat)width
+        aligment:(NSTextAlignment)aligment
+           color:(UIColor*)color
+            font:(UIFont*)font;
+
+
+/**
+ 画矩形
+
+ @param rect 位置
+ @param style 风格
+ @param color 颜色
+ @param lineWith 线条宽度(仅对LZDrawStyleNormal模式有效)
+ */
+- (void)drawRect:(CGRect)rect
+           style:(LZDrawStyle)style
+           color:(UIColor*)color
+        lineWith:(CGFloat)lineWith;
+
+// 画圆
+- (void)drawCircle:(CGRect)rect
+             style:(LZDrawStyle)style
+             color:(UIColor*)color
+          lineWith:(CGFloat)lineWith;
+
+// 画弧
+- (void)drawArc:(CGRect)rect
+       location:(CGPoint)location
+           form:(CGPoint)from
+             to:(CGPoint)to
+         radius:(CGFloat)radius
+          style:(LZDrawStyle)style
+          color:(UIColor*)color
+       lineWith:(CGFloat)lineWith;
+
+// 画图
+- (void)drawImage:(CGRect)frame image:(UIImage*)image;
 
 @end
--- Example/Pods/LZCommonSDK/UIView+LZDraw.m
+++ Example/Pods/LZCommonSDK/UIView+LZDraw.m
@@ -33,5 +33,61 @@
     CGContextDrawPath(context, kCGPathFillStroke);
 }
 
+- (void)drawRect:(CGRect)rect style:(LZDrawStyle)style color:(UIColor*)color lineWith:(CGFloat)lineWith
+{
+     CGContextRef context = UIGraphicsGetCurrentContext();
+     CGContextSetRGBFillColor(context, color.red, color.green, color.blue, color.alpha);
+     if (style == LZDrawStyleNormal) {
+         CGContextSetLineWidth(context, lineWith);
+         CGContextAddRect(context, rect);
+     }
+     else{
+         CGContextFillRect(context, rect);
+     }
+     CGContextStrokePath(context);
+}
+
+- (void)drawCircle:(CGRect)rect style:(LZDrawStyle)style color:(UIColor*)color lineWith:(CGFloat)lineWith
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetRGBStrokeColor(context, color.red, color.green, color.blue, color.alpha);
+    if (style == LZDrawStyleNormal) {
+        CGContextSetLineWidth(context, lineWith);
+        CGContextAddEllipseInRect(context, rect);
+    }
+    else{
+        CGContextFillEllipseInRect(context, rect);
+    }
+    CGContextDrawPath(context, kCGPathStroke);
+}
+
+- (void)drawArc:(CGRect)rect
+       location:(CGPoint)location
+           form:(CGPoint)from
+             to:(CGPoint)to
+         radius:(CGFloat)radius
+          style:(LZDrawStyle)style
+          color:(UIColor*)color
+       lineWith:(CGFloat)lineWith
+{
+     CGContextRef context = UIGraphicsGetCurrentContext();
+     CGContextSetRGBStrokeColor(context, color.red, color.green, color.blue, color.alpha);
+     CGContextSetLineWidth(context, lineWith);
+     CGContextSetStrokeColorWithColor(context, color.CGColor);
+     CGContextMoveToPoint(context, location.x, location.y);
+    CGContextAddArcToPoint(context, from.x, from.y, to.x, to.y, radius);
+     if (style == LZDrawStyleNormal) {
+         CGContextStrokePath(context);
+     }
+     else{
+         CGContextFillPath(context);//实心的
+     }
+}
+
+- (void)drawImage:(CGRect)frame image:(UIImage*)image
+{
+    [image drawInRect:frame];
+}
+
 
 @end
