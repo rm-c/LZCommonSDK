50ab6ee16f821531cdaae637f82f1b743fd8ecc0
Macye <linzhenwei8888@gmail.com>
2018-08-27 10:57:21 +0800

    新功能

---
+++ Example/Pods/LZCommonSDK/UIView+LZDraw.h
@@ -0,0 +1,35 @@
+//
+//  UIView+LZDraw.h
+//  LZCommonSDK
+//
+//  Created by Macye on 2018/8/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LZDraw)
+
+
+/**
+ 画直线
+
+ @param from 起点
+ @param to 终点
+ @param color 颜色
+ @param width 线条宽度
+ */
+- (void)drawLineFrom:(CGPoint)from to:(CGPoint)to color:(UIColor*)color with:(CGFloat)width;
+
+
+/**
+ 画文本
+
+ @param text 文本
+ @param frame 位置
+ @param aligment 对齐格式
+ @param color 颜色
+ @param font 字体
+ */
+- (void)drawText:(NSString*)text inFrame:(CGRect)frame aligment:(NSTextAlignment)aligment color:(UIColor*)color font:(UIFont)font;
+
+@end
---
+++ Example/Pods/LZCommonSDK/UIView+LZDraw.m
@@ -0,0 +1,37 @@
+//
+//  UIView+LZDraw.m
+//  LZCommonSDK
+//
+//  Created by Macye on 2018/8/27.
+//
+
+#import "UIView+LZDraw.h"
+#import "UIColor+LZHelper.h"
+
+@implementation UIView (LZDraw)
+
+- (void)drawLineFrom:(CGPoint)from to:(CGPoint)to color:(UIColor*)color with:(CGFloat)width
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, width);
+    CGContextSetRGBStrokeColor(context, color.red, color.green, color.blue, color.alpha);//线条颜色
+    CGContextMoveToPoint(context, from.x, from.y);
+    CGContextAddLineToPoint(context, to.x, to.y);
+    CGContextDrawPath(context, kCGPathStroke);
+}
+
+- (void)drawText:(NSString*)text inFrame:(CGRect)frame aligment:(NSTextAlignment)aligment color:(UIColor*)color font:(UIFont)font
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, 0.25);
+    NSMutableParagraphStyle *paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    paragraphStyle.alignment = aligment;
+    [text drawInRect:frame withAttributes:@{NSFontAttributeName:font,
+                                           NSForegroundColorAttributeName:color,
+                                           NSParagraphStyleAttributeName: paragraphStyle}];
+    CGContextSetLineWidth(context, 0.5);
+    CGContextDrawPath(context, kCGPathFillStroke);
+}
+
+
+@end
