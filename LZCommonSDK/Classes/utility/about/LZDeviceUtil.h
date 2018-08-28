e6f2b8ea4b1a2b89551f273a2d2464fdbc21c092
Macye <linzhenwei8888@gmail.com>
2018-08-28 17:40:44 +0800

    zhenwei

--- LZCommonSDK/Classes/utility/about/LZDeviceUtil.h
+++ LZCommonSDK/Classes/utility/about/LZDeviceUtil.h
@@ -12,13 +12,7 @@
 
 + (NSUInteger)getDeviceSystemMajorVersion;  // 获取系统iOS主版本号
 + (NSString*)getDeviceSystemVersionString;  // 获取系统iOS版本号
-
-/**
- *  获取设备机型
- *
- *  @return 机型
- */
-+ (NSString*)getDeviceModel;
++ (NSString*)getDeviceModel;    // 获取设备机型
 
 /**
  *  获取屏幕分辨率
@@ -26,19 +20,8 @@
  *  @return 屏幕分辨率 eg:320*480
  */
 + (NSString*)getDeviceScreenResolution;
-
-/**
- *  获取设备名称
- *
- *  @return 设备名称
- */
-+ (NSString*)getDeviceName;
-
-/**
- *  根据Keychain文件,获取设备UUID
- *
- *  @return 设备唯一设备号
- */
-+ (NSString*)getDeviceUUID;
++ (NSString*)getDeviceName; // 获取设备名称
++ (NSString*)getDeviceUUID; // 根据Keychain文件,获取设备UUID
++ (void)setDeviceInterfaceOrientation:(UIInterfaceOrientation)orientation;      // 设置屏幕方向
 
 @end
--- LZCommonSDK/Classes/utility/about/LZDeviceUtil.m
+++ LZCommonSDK/Classes/utility/about/LZDeviceUtil.m
@@ -221,4 +221,25 @@
     return totalspace;
 }
 
+/*!
+ *  强制锁定屏幕方向
+ *
+ *  @param orientation 屏幕方向
+ */
++ (void)setDeviceInterfaceOrientation:(UIInterfaceOrientation)orientation
+{
+    // arc下
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)])
+    {
+        SEL selector             = NSSelectorFromString(@"setOrientation:");
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[UIDevice instanceMethodSignatureForSelector:selector]];
+        [invocation setSelector:selector];
+        [invocation setTarget:[UIDevice currentDevice]];
+        int val                  = orientation;
+        // 从2开始是因为0 1 两个参数已经被selector和target占用
+        [invocation setArgument:&val atIndex:2];
+        [invocation invoke];
+    }
+}
+
 @end
