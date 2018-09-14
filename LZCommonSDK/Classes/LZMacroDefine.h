4252ee1ef99cdc181bc2b93d189086420388407f
Macye <linzhenwei8888@gmail.com>
2018-09-14 17:14:08 +0800

    zhenwei

--- LZCommonSDK/Classes/LZMacroDefine.h
+++ LZCommonSDK/Classes/LZMacroDefine.h
@@ -60,8 +60,8 @@
 //空值判断
 #define lz_IsEmpty(thing)                      [LZCommonUtil isEmpty:(thing)]
 #define lz_IsNotEmpty(thing)                   [LZCommonUtil isNotEmpty:(thing)]
-#define lz_SAFE_STR(STR)                       ((STR)&&(![STR isEqual:[NSNull null]]) ? (STR) : @"")
-#define lz_SAFE_NUM(NUM)                       ((NUM)&&(![NUM isEqual:[NSNull null]]) ? (NUM) : @(0))
+#define lz_SafeStr(STR)                       ((STR)&&(![STR isEqual:[NSNull null]]) ? (STR) : @"")
+#define lz_SafeNum(NUM)                       ((NUM)&&(![NUM isEqual:[NSNull null]]) ? (NUM) : @(0))
 
 #define lz_IsRespondsToSelector(target, selectorName)  ((target) && [(target) respondsToSelector:NSSelectorFromString(selectorName)])
 
