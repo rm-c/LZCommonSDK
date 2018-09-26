8b89c345dd297d1db9c169f9f666998fbda224f5
Macye <linzhenwei8888@gmail.com>
2018-09-26 17:25:53 +0800

    Zhenwei

--- LZCommonSDK/Classes/LZMacroDefine.h
+++ LZCommonSDK/Classes/LZMacroDefine.h
@@ -50,8 +50,8 @@
 #define LzUIColorFromRGB(rgbValue)            [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
                                                             green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
                                                              blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-#define LzUIColorFromR_G_B_A(r,g,b,a)         [UIColor colorWithRed:(r) / 255.0 green:(g) / 255.0 blue:(b) / 255.0 alpha:a]
-#define LzUIColorFromR_G_B(r,g,b)             LzUIColorFromR_G_B_A(r,g,b,1.0)
+#define LZColorFromR_G_B_A(r,g,b,a)         [UIColor colorWithRed:(r) / 255.0 green:(g) / 255.0 blue:(b) / 255.0 alpha:a]
+#define LZColorFromR_G_B(r,g,b)             LZColorFromR_G_B_A(r,g,b,1.0)
 
 //文件操作相关
 #define LzFilePathInSandBox(fileName)         [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES) objectAtIndex:0] stringByAppendingPathComponent:fileName]
